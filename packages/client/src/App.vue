37cc10ade53f68bab274bd59d38771bb5b25954c
mimof <mimo.feng@gmail.com>
2022-10-11 15:40:02 +0800

    delete hello world

--- packages/client/src/App.vue
+++ packages/client/src/App.vue
@@ -5,16 +5,16 @@ import HelloWorld from "./components/HelloWorld.vue";
 
 <template>
   <header>
-    <img
+    <!-- <img
       alt="Vue logo"
       class="logo"
       src="@/assets/logo.svg"
       width="125"
       height="125"
-    />
+    /> -->
 
     <div class="wrapper">
-      <HelloWorld msg="welcome to freins!" />
+      <!-- <HelloWorld msg="welcome to freins!" /> -->
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
