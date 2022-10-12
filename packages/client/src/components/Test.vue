1b411c831925414d2bdace0a6229c1d1166440f9
mimof <mimo.feng@gmail.com>
2022-10-12 11:58:41 +0800

    dev host listen network

--- packages/client/src/components/Test.vue
+++ packages/client/src/components/Test.vue
@@ -1,2 +1,22 @@
-<script setup lang="ts"></script>
-<template>Test</template>
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+
+const canvas = ref<HTMLCanvasElement>();
+
+onMounted(() => {
+  if (canvas.value) {
+    console.log(canvas.value);
+  }
+});
+</script>
+
+<template>
+  <canvas width="1000" height="1000" ref="canvas"></canvas>
+</template>
+
+<style scoped>
+canvas {
+  width: 500px;
+  height: 500px;
+}
+</style>
--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -11,4 +11,9 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  server: {
+    // 指定服务器应该监听哪个 IP 地址。 如果将此设置为 0.0.0.0 或者 true 将监听所有地址，包括局域网和公网地址。
+    // 也可以通过 CLI 使用 --host 0.0.0.0 或 --host 来设置。
+    host: "0.0.0.0",
+  },
 });
